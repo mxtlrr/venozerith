a0ad1c89568981d4f36a77298d1b8713d31ad17e
mxtlrr <ruson0914@gmail.com>
2024-05-22 21:56:58 -0500

    cool it delays now

--- src/main.c
+++ src/main.c
@@ -1,11 +1,24 @@
 #include <raylib.h>
 #include <stdio.h>
+#include <math.h>
 
 Sound sounds[1360] = { 0 }; // 240 - 1600.
+
+#define TRACK_LEN 0.38f
+
 int currentSound = 0;
 void _(){}
 
+// Finds the delay required between each drum hit.
+// Input
+//      -> BPM, beats per minute
+int findDelaySec(int bpm){
+    return round(1000/round(((bpm/60)*(1/TRACK_LEN))));
+}
+
 int main(void){
+    char* VERSION = "0.0.1";
+
     SetTraceLogCallback(_);
     InitWindow(300, 250, "Venozerith Drum Minion");
     InitAudioDevice();
@@ -34,8 +47,17 @@ int main(void){
         char* playing = playing_sound ? "Playing" : "Not playing";
 
         if(playing_sound){
+            int DelayMillisec = findDelaySec(bpm);
+            float delay = (float)((float)DelayMillisec/1000);
+
+            #ifdef DEBUG
+                printf("BPM: %d | DELAY (ms): %d | WAITING: %.5f\n", bpm, DelayMillisec, delay);
+            #endif
+
+            // This is why we do above 240 BPM. I'm not a big fan of
+            // delays.
+            WaitTime(delay);
             PlaySound(sounds[currentSound]);
-            // TODO: get BPM and wait however many seconds between each hit
             currentSound++;
 
             // Reset
@@ -44,6 +66,7 @@ int main(void){
         BeginDrawing();
             ClearBackground(WHITE);
             DrawText(TextFormat("BPM: %d\n%s", bpm, playing), 10, 10, 20, BLACK);
+            DrawText(TextFormat("Venozerith v%s", VERSION), 10, 230, 10, GRAY);
         EndDrawing();
     }
     for (int i = 1; i < 1360; i++)
