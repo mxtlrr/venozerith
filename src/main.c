9fb4f0ecb3845563fdbc43bc6493eee14999ea50
mxtlrr <ruson0914@gmail.com>
2024-06-08 09:03:42 -0500

    Remove tab 4

--- src/main.c
+++ src/main.c
@@ -8,10 +8,7 @@
 #include "sound/sound.h"
 #include "tabs/midi.h"
 
-char* tabs[] = { "Reserved", "Manual Sounds", "MIDI Editor", "Settings",
-								 "Snare Spam" };
-
-Sound sounds[1360] = { 0 };
+char* tabs[] = { "Reserved", "Manual Sounds", "MIDI Editor", "Settings" };
 
 void _(){}
 int main(void) {
@@ -28,13 +25,7 @@ int main(void) {
 
 	int tab = 1; // Current tab we're on
 
-	bool spam_yes = false;
 	int bpm = 240;
-	sounds[0] = s.sounds[0];
-	for(int i = 1; i < 1360; i++){
-		sounds[i] = LoadSoundAlias(sounds[0]);
-	}
-  int currentSound = 0;
 	while(!WindowShouldClose()){
 		
 		// Stupid code to change tabs
@@ -98,39 +89,9 @@ int main(void) {
 						starting_y += 30;
 					}
 					break;
-
-				// Spam drums
-				case 4:
-					// Active indicator
-					DrawText(tabs[1], 10, 450, 20, GRAY);
-					DrawText(tabs[2], 175, 450, 20, GRAY);
-					DrawText(tabs[3], 300, 450, 20, GRAY);
-					DrawText(tabs[4], 400, 450, 20, BLACK);
-
-					DrawText(TextFormat("Press '%c' to start spamming (BPM=%d)\nSpamming? %s", keys[k.keys[0].key],
-									bpm, spam_yes?"Yes":"No"), 50, 50, 20, BLACK);
-					DrawText("\n\n\n\n+/- to increase/decrease BPM by 10", 50, 50, 20, BLACK);
-
-					if(IsKeyPressed(get_ordinal(keys[k.keys[0].key]))) spam_yes = !spam_yes;
-					if(IsKeyPressed(KEY_MINUS) && bpm > 240) bpm -= 10;
-					if(IsKeyPressed(KEY_EQUAL)) bpm += 10;
-
-
-					if(spam_yes){
-						int dmesc = get_len(bpm, 0.38f);
-						float delay = (float)((float)dmesc/1000);
-
-						WaitTime(delay);
-						PlaySound(sounds[currentSound]);
-						currentSound++;
-					}
 			}
 		EndDrawing();
 	}
-
-	for (int i = 1; i < 1360; i++)
-			UnloadSoundAlias(sounds[i]);     // Unload sound aliases
-	UnloadSound(sounds[0]);
 	
 	for(int i = 0; i < 3; i++){
 		UnloadSound(s.sounds[i]);
