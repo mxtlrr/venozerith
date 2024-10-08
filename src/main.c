6e203b73cd899d1697073570078b6d700c3f1187
mxtlrr <ruson0914@gmail.com>
2024-10-08 18:03:12 -0500

    indicator for tabs

--- src/main.c
+++ src/main.c
@@ -5,17 +5,38 @@
 
 void _(){}
 
+int current_tab = 0;
+char* tabs[] = {
+	"Reserved", "Keybinds", "MIDI Editor",
+	"Sandbox", "Reserved"
+};
+
 int main(void){
   SetTraceLogCallback(_);
 
 	find_samples("/home/nick/music/hof");
 
-	return 0;
   InitWindow(800, 600, "Venozerith");
-  SetTargetFPS(240); // ?
+  SetTargetFPS(240); // Eventually change this
 
   while(!WindowShouldClose()){
+		// Tab swapping.
+		int get_k = GetKeyPressed();
+		if(get_k >= 49 && get_k <= 54){
+			current_tab = (get_k - 48);
+		}
+
     BeginDrawing();
+			// TODO: stuff for specific tabs
+
+
+			// Draw the tabs (indicator)
+			int q = 10;
+			for(int i = 0; i < 5; i++){
+				if(current_tab == i+1) DrawText(TextFormat("%s", tabs[i]), q, 10, 20, BLACK);
+				else DrawText(TextFormat("%s", tabs[i]), q, 10, 20, GRAY);
+				q += 140;
+			}
 
       // Status bar
       DrawText(TextFormat("git-%s", GIT_HASH), 10, 580, 10, BLACK);
